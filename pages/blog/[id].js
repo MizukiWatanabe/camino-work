f9cf469819abb12f986af78addddaf8a7220dbdb
mw <ni4nte@gmail.com>
2022-07-27 12:19:16 +0900

    記事取得、404追加

---
+++ pages/blog/[id].js
@@ -0,0 +1,43 @@
+import { client } from '../../libs/client';
+
+export default function BlogId({ blogs }) {
+  const date = blogs.publishedAt;
+  const formatDate = new Date(date);
+  const test =
+    String(formatDate.getFullYear()) +
+    String(formatDate.getMonth() - 1) +
+    String(formatDate.getDate());
+  // console.log(formatDate);
+  return (
+    <main>
+      <h1>{blogs.title}</h1>
+      <p>{blogs.publishedAt}</p>
+      <div>{test}</div>
+      <div
+        dangerouslySetInnerHTML={{
+          __html: `${blogs.body}`,
+        }}
+      />
+    </main>
+  );
+}
+
+// 静的生成のためのパスを指定します
+export const getStaticPaths = async () => {
+  const data = await client.get({ endpoint: 'blog' });
+
+  const paths = data.contents.map((content) => `/blog/${content.id}`);
+  return { paths, fallback: false };
+};
+
+// データをテンプレートに受け渡す部分の処理を記述します
+export const getStaticProps = async (context) => {
+  const id = context.params.id;
+  const data = await client.get({ endpoint: 'blog', contentId: id });
+
+  return {
+    props: {
+      blogs: data,
+    },
+  };
+};
